d41606ef9a4d731385847052d9364921e1a9efec
Ahmad El Gamal <ahmadelgamal@gmail.com>
2020-12-20 20:18:59 -0800

    Completes most of structure'

---
+++ models/Thought.js
@@ -0,0 +1,12 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ThoughtSchema = new Schema(
+  {
+
+  }
+);
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
